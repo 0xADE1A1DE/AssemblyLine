70f1a8d27628cdc903d03d6a65ea135621c36762
daviduwu9 <daviduwu@hotmail.com>
2021-09-16 04:03:18 +0930

    updated comments for instr_table to match readme

--- src/instructions.h
+++ src/instructions.h
@@ -37,14 +37,14 @@ const static struct {
 // defines opcode layout of each supported instruction
 struct instr_table{
 
-  /* null terminated string representation of an instruction ex: "mov"
+  /* null-terminated string representation of an instruction ex: "mov"
    * subsequent instructions of the same name with a different operand
-   * encoding will be place contiguously with the first instance of the
+   * encoding will be placed contiguously with the first instance of the
    * instuction and will have the '\0' string
    */
   char instr_name[MAX_INSTR_LEN];
 
-  // asm_instr enumerator for uniquely identifying a single instruction
+  // asm_instr enumerator for uniquely identifying a instruction
   int name;
 
   /* contains the valid operand formats for an instruction that maps
@@ -60,9 +60,9 @@ struct instr_table{
   operand_encoding encode_operand;
 
   /* enumerator for defining the semantic type of an instruction
-   * if the instruction type is not known set this value to 'OTHER'
-   * refer to the link below to find the correct type for the instruction
-   * https://docs.oracle.com/cd/E36784_01/html/E36859/eoizp.html#scrolltoc
+   * where special encoding is required ( currently, only applicable for
+   * SHIFT, DATA_TRANSFER, and CONTROLFLOW type instructions)
+   * else set this to 'OTHER'
    */
   instr_type type;
 
@@ -73,18 +73,23 @@ struct instr_table{
   int op_offset_i;
 
   /* 'i' index of opcode[i] when an offset is present for a REG value denoted as
-   * '+ rd' in felixcloutier (set this value to NA if not applicable to the instruction)
+   * '+ rd' in the intel manual section 3.1.1.1
+   * (set this value to NA if not applicable to the instruction)
    */
   int rd_offset_i;
 
-  // used instructions with a single register operand denoted as '/num' in felixcloutier
+  /* used for instructions with a single register operand denoted as '/digit'
+   * in the intel manual section 3.1.1.1
+   * (set this value to NA if not applicable to the instruction)
+   */
   int single_reg_r;
 
   // number of bytes in the opcode[MAX_OPCODE_LEN] field
   int instr_size;
 
   /* displacement for the W0 prefix (following byte after the vector extension
-   * prefix VEX)
+   * prefix VEX) check intel manual section 3.1.1.2 (set this value to NA if not
+   * applicable to the instruction)
    */
   int w0_disp;
 
