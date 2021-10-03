8b236454aa4dd3e2e9b9419bb8f8d25dd02e49dd
daviduwu9 <daviduwu@hotmail.com>
2021-10-04 06:38:44 +1030

    added test case for divpd

--- test/gp_to_vector_register.asm
+++ test/gp_to_vector_register.asm
@@ -1,13 +1,14 @@
 SECTION .text
 GLOBAL test
 test:
-mov rcx, 0x11111111 
-mov r12, 0x22222222
+mov rcx, 0x6 
+mov r12, 0x2
 movq xmm4, rcx
 movq xmm7, r12
-movd xmm1, ecx
-movd xmm0, r12d
-paddq xmm4, xmm7
+cvtdq2pd xmm4, xmm4
+cvtdq2pd xmm7, xmm7
+divpd xmm4, xmm7
 movq xmm5, xmm4
+cvtpd2dq xmm5, xmm5
 movq rax, xmm5
 ret
