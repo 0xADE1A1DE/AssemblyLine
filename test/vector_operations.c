947b0497d91b5363dd2a9d90926772b2c0a7adf7
daviduwu9 <daviduwu@hotmail.com>
2021-12-09 21:13:32 +1030

    added test cases for vsubpd, vmulpd, and vdivpd

--- test/vector_operations.c
+++ test/vector_operations.c
@@ -23,38 +23,119 @@
 #include <string.h>
 #include <sys/mman.h>
 
-int execute_test(void (*exe)(double *, double *, double *)) {
+#define ADD 0
+#define SUB 1
+#define MUL 2
+#define DIV 3
+
+int execute_test(void (*exe)(double *, double *, double *), int type) {
 
   double A[] = {1.0, 2.0, 3.0, 4.0};
   double B[] = {1.0, 2.0, 3.0, 4.0};
   double C[] = {1.0, 1.0, 1.0, 1.0};
   exe(A, B, C);
-  for (int it = 0; it < 4; it++) {
-    if (C[it] != A[it] + B[it])
-      return EXIT_FAILURE;
+  switch (type) {
+  case ADD:
+    for (int it = 0; it < 4; it++)
+      if (C[it] != A[it] + B[it])
+        return EXIT_FAILURE;
+    break;
+  case SUB:
+    for (int it = 0; it < 4; it++)
+      if (C[it] != A[it] - B[it])
+        return EXIT_FAILURE;
+    break;
+  case MUL:
+    for (int it = 0; it < 4; it++)
+      if (C[it] != A[it] * B[it])
+        return EXIT_FAILURE;
+    break;
+  case DIV:
+    for (int it = 0; it < 4; it++)
+      if (C[it] != A[it] / B[it])
+        return EXIT_FAILURE;
+    break;
+  default:
+    return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
 }
 
 int main(int argc, char **argv) {
 
-  const char *add_xmm = "vmovupd ymm0, [rdi]\n"
+  const char *add_ymm = "vmovupd ymm0, [rdi]\n"
                         "vmovupd ymm1, [rsi]\n"
                         "vaddpd ymm3, ymm0, ymm1\n"
                         "vmovupd [rdx], ymm3\n"
                         "ret\n";
- 
-  assemblyline_t asm_exe = asm_create_instance(NULL, 0);
-  if (assemble_str(asm_exe, add_xmm) == EXIT_FAILURE)
+
+  const char *sub_ymm = "vmovupd ymm0, [rdi]\n"
+                        "vmovupd ymm1, [rsi]\n"
+                        "vsubpd ymm3, ymm0, ymm1\n"
+                        "vmovupd [rdx], ymm3\n"
+                        "ret\n";
+
+  const char *mul_ymm = "vmovupd ymm0, [rdi]\n"
+                        "vmovupd ymm1, [rsi]\n"
+                        "vmulpd ymm3, ymm0, ymm1\n"
+                        "vmovupd [rdx], ymm3\n"
+                        "ret\n";
+
+  const char *div_ymm = "vmovupd ymm0, [rdi]\n"
+                        "vmovupd ymm1, [rsi]\n"
+                        "vdivpd ymm3, ymm0, ymm1\n"
+                        "vmovupd [rdx], ymm3\n"
+                        "ret\n";
+
+  assemblyline_t al = asm_create_instance(NULL, 0);
+  if (assemble_str(al, add_ymm) == EXIT_FAILURE)
     return EXIT_FAILURE;
 
   // type cast to function pointer
-  void (*test_add)(double * A, double * B, double * C) =
-      (void (*)(double *, double *, double *))(asm_get_code(asm_exe));
-      
-  if (execute_test(test_add)) {
-    fprintf(stderr, "test_add.asm did not produce expected results\n");
+  void (*test_op)(double *A, double *B, double *C) = asm_get_code(al);
+
+  if (execute_test(test_op, ADD)) {
+    fprintf(stderr, "ADD did not produce expected results\n");
     return EXIT_FAILURE;
   }
+  // clear previous test
+  asm_set_offset(al, 0);
+
+  if (assemble_str(al, sub_ymm) == EXIT_FAILURE)
+    return EXIT_FAILURE;
+
+  test_op = asm_get_code(al);
+
+  if (execute_test(test_op, SUB)) {
+    fprintf(stderr, "SUBTRACT did not produce expected results\n");
+    return EXIT_FAILURE;
+  }
+
+  // clear previous test
+  asm_set_offset(al, 0);
+
+  if (assemble_str(al, mul_ymm) == EXIT_FAILURE)
+    return EXIT_FAILURE;
+
+  test_op = asm_get_code(al);
+
+  if (execute_test(test_op, MUL)) {
+    fprintf(stderr, "MULTIPLY did not produce expected results\n");
+    return EXIT_FAILURE;
+  }
+
+  // clear previous test
+  asm_set_offset(al, 0);
+
+  if (assemble_str(al, div_ymm) == EXIT_FAILURE)
+    return EXIT_FAILURE;
+
+  test_op = asm_get_code(al);
+
+  if (execute_test(test_op, DIV)) {
+    fprintf(stderr, "DIVIDE did not produce expected results\n");
+    return EXIT_FAILURE;
+  }
+
   return EXIT_SUCCESS;
 }
