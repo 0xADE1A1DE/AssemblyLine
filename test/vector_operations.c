ad171bfadf90fdc8c552b39b56307f7ebd5f85e0
daviduwu9 <daviduwu@hotmail.com>
2021-12-10 00:16:35 +1030

    added test case for vpsubq

--- test/vector_operations.c
+++ test/vector_operations.c
@@ -124,6 +124,12 @@ const char *add_long_ymm = "vmovdqu ymm0, [rdi]\n"
                            "vmovdqu [rdx], ymm3\n"
                            "ret\n";
 
+const char *sub_long_ymm = "vmovdqu ymm0, [rdi]\n"
+                           "vmovdqu ymm1, [rsi]\n"
+                           "vpsubq ymm3, ymm0, ymm1\n"
+                           "vmovdqu [rdx], ymm3\n"
+                           "ret\n";
+
 int main(int argc, char **argv) {
 
   assemblyline_t al = asm_create_instance(NULL, 0);
@@ -185,6 +191,19 @@ int main(int argc, char **argv) {
 
   if (execute_long_test(test_long, ADD)) {
     fprintf(stderr, "ADD long did not produce expected results\n");
+    return EXIT_FAILURE;
+  }
+
+  // clear previous test
+  asm_set_offset(al, 0);
+
+  if (assemble_str(al, sub_long_ymm) == EXIT_FAILURE)
+    return EXIT_FAILURE;
+
+  test_long = asm_get_code(al);
+
+  if (execute_long_test(test_long, SUB)) {
+    fprintf(stderr, "SUBTRACT long did not produce expected results\n");
     return EXIT_FAILURE;
   }
 
